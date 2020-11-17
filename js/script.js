814e90bd2cea409218ad419fec965ac599824295
francesco <f.iacono89@gmail.com>
2020-11-17 16:29:39 +0100

    fine javascript

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 // FizzBuzz
 // Scrivi un programma che stampi i numeri da 1 a 100,
-var numeri = 0;
-
+var numero = 0;
 for (var i = 0; i < 100; i++) {
-  numeri = i + 1;
-  console.log(numeri);
+  numero = i + 1;
+  console.log(numero);
+  if ((numero % 3 == 0) && (numero % 5 == 0 )) {
+      numero = numero + ' FizzBuzz';
+      document.getElementById('numeri-line').innerHTML += numero + "<br>";
+      console.log(numero);
+  } else if (numero % 5 == 0 ) { //i multipli di 5 stampi Buzz
+      numero = numero + ' Buzz';
+      document.getElementById('numeri-line').innerHTML += numero + "<br>";
+      console.log(numero);
+  } else if (numero % 3 == 0) { //i multipli di 3 stampi “Fizz”
+      numero = numero + ' Fizz';
+      document.getElementById('numeri-line').innerHTML += numero + "<br>";
+      console.log(numero);
+  } else {
+      document.getElementById('numeri-line').innerHTML += numero + "<br>";
+  }
 }
-
-// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Come faccio a sapere se un numero è divisibile per?
